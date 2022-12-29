8e5f2b00e766731fd02ed8dbc3513fa97629aeda
metehaansever <metehaansever@gmail.com>
2022-12-29 02:06:45 +0100

    _app.js file removed and font awesome package move into _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,6 +2,9 @@ import '@/assets/base.css';
 import { Layout } from '@/components/Layout';
 import { ThemeProvider } from 'next-themes';
 import { Toaster } from 'react-hot-toast';
+import '@fortawesome/fontawesome-svg-core/styles.css'; // import Font Awesome CSS
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
 export default function MyApp({ Component, pageProps }) {
   return (
